d92bb58d50b1940d595f9a1b42adce6a35c5982f
Lorna Timbah <1225738+webgrrrl@users.noreply.github.com>
2024-05-21 20:55:51 +0800

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== This Is It Product Schema ===
+Contributors: webgrrrl
+Tags: woocommerce, product, seo, json
+Requires at least: 5
+Tested up to: 5.4
+Stable tag: 1.0
+Requires PHP: 8.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Adds JSON-LD product schema markup to WooCommerce product pages.
+
+== Description ==
+
+Google with all the essential details it craves. Think things like:
+
+* Product name and description
+* Price and availability
+* Images
+
+Brand information: Shows who's behind the amazing product.
+
+Ratings and reviews: Social proof is powerful, and this lets customers know others love your products.
+
+Basically, This Is It Product Schema provides a complete package for your product schema needs. It translates all that WooCommerce product information into a language that Google understands, ultimately improving your SEO game.
+
+== Frequently Asked Questions ==
+
+= What do I need to set up once the plugin is activated? =
+
+This plugin requires NO setup once activated.
+
+== Changelog ==
+
+= 1.0 =
+* First version of this plugin.
---
+++ this-is-it-product-schema.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Plugin Name: This Is It Product Schema
+ * Plugin Description: Adds JSON-LD product schema markup to WooCommerce product pages.
+ * Plugin URI: https://webgrrrl.net/archives/this-is-it-product-schema-plugin-woocommerce-google-shopping.htm
+ * Author: webgrrrl
+ * Author URI: http://webgrrrl.net
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Check for WooCommerce
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+
+  add_action( 'wp_head', 'tis_product_schema' ); // Updated function name
+
+  function tis_product_schema() {
+    if ( is_singular( 'product' ) ) {
+      global $product;
+
+      $schema = array(
+        '@context' => 'https://schema.org',
+        '@type' => 'Product',
+        'name' => $product->get_name(),
+        'description' => $product->get_description(),
+        'url' => get_permalink(),
+        'brand' => array(
+          '@type' => 'Brand',
+          'name' => get_the_brand(), // Assuming you have a function to get the brand name
+        ),
+        'image' => get_the_post_thumbnail_url( get_the_ID(), 'full' ),
+      );
+
+      if ( $product->is_type( 'variable' ) ) {
+        $variations = $product->get_available_variations();
+        $schema['offers'] = array();
+
+        foreach ( $variations as $variation ) {
+          $variation_id = $variation->get_id();
+          $variation_data = wc_get_product_variation_attributes( $variation_id );
+
+          $color = ''; // Assuming color data is stored in an attribute named 'color'
+          if ( isset( $variation_data['color'] ) ) {
+            $color = $variation_data['color'];
+          }
+
+          $schema['offers'][] = array(
+            '@type' => 'Offer',
+            'price' => $variation->get_price(),
+            'priceCurrency' => get_woocommerce_currency(),
+            'availability' => $variation->is_in_stock() ? 'InStock' : 'OutStock',
+            'sku' => $variation->get_sku(),
+            'url' => get_permalink( $variation_id ), // Variation URL
+            'name' => $product->get_name() . ' - ' . $color, // Include color in product name
+          );
+        }
+      } else {
+        $schema['sku'] = $product->get_sku();
+        $schema['offers'] = array(
+          '@type' => 'Offer',
+          'price' => $product->get_price(),
+          'priceCurrency' => get_woocommerce_currency(),
+          'availability' => $product->is_in_stock() ? 'InStock' : 'OutStock',
+        );
+      }
+
+      echo '<script type="application/ld+json">' . json_encode( $schema ) . '</script>';
+    }
+  }
+}
